a21fc162e9cb95e4a6caca08f9b21a1d3c3339ed
karyllecatubay <karyllecatubigcatubay@gmail.com>
2025-03-21 22:42:07 +0800

    Update CalculatorForm.Designer.cs

--- CalculatorForm.Designer.cs
+++ CalculatorForm.Designer.cs
@@ -1,4 +1,4 @@
-namespace BasicCalculator
+namespace EnhancedCalculator
 {
     partial class CalculatorForm
     {
@@ -31,17 +31,28 @@ namespace BasicCalculator
             this.components = new System.ComponentModel.Container();
             
             // Form properties
-            this.ClientSize = new System.Drawing.Size(350, 500);
+            this.ClientSize = new System.Drawing.Size(350, 550);
             this.Text = "Calculator";
             this.BackColor = System.Drawing.Color.Black;
             this.Name = "CalculatorForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            
+            // Expression label (shows the current calculation)
+            this.expressionLabel = new System.Windows.Forms.Label();
+            this.expressionLabel.Location = new System.Drawing.Point(25, 15);
+            this.expressionLabel.Size = new System.Drawing.Size(300, 20);
+            this.expressionLabel.Font = new System.Drawing.Font("Arial", 10F);
+            this.expressionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.expressionLabel.Name = "expressionLabel";
+            this.expressionLabel.ForeColor = System.Drawing.Color.LightGray;
             
             // Display textbox
             this.displayTextBox = new System.Windows.Forms.TextBox();
-            this.displayTextBox.Location = new System.Drawing.Point(25, 25);
+            this.displayTextBox.Location = new System.Drawing.Point(25, 40);
             this.displayTextBox.Size = new System.Drawing.Size(300, 50);
-            this.displayTextBox.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold);
+            this.displayTextBox.Font = new System.Drawing.Font("Arial", 26F, System.Drawing.FontStyle.Bold);
             this.displayTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             this.displayTextBox.ReadOnly = true;
             this.displayTextBox.Text = "0";
@@ -51,23 +62,24 @@ namespace BasicCalculator
             this.displayTextBox.ForeColor = System.Drawing.Color.White;
             
             // Create buttons
-            string[,] buttonLabels = new string[5, 4] {
+            string[,] buttonLabels = new string[6, 4] {
+                { "MC", "MR", "M-", "M+" },
                 { "7", "8", "9", "÷" },
                 { "4", "5", "6", "×" },
                 { "1", "2", "3", "-" },
                 { "0", ".", "C", "+" },
-                { "MC", "MR", "M-", "=" }
+                { "±", "DEL", "CE", "=" }
             };
             
             // Button layout
             int buttonWidth = 70;
-            int buttonHeight = 70;
+            int buttonHeight = 60;
             int xPadding = 5;
             int yPadding = 5;
             int xStart = 25;
-            int yStart = 100;
+            int yStart = 110;
             
-            for (int row = 0; row < 5; row++)
+            for (int row = 0; row < 6; row++)
             {
                 for (int col = 0; col < 4; col++)
                 {
@@ -76,10 +88,11 @@ namespace BasicCalculator
                                                       yStart + row * (buttonHeight + yPadding));
                     button.Size = new System.Drawing.Size(buttonWidth, buttonHeight);
                     button.Text = buttonLabels[row, col];
-                    button.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+                    button.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold);
                     button.Name = buttonLabels[row, col] + "Button";
                     button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
                     button.FlatAppearance.BorderSize = 0;
+                    button.TabStop = false; // Prevent focus rectangle
                     
                     // Set button colors based on type
                     if (buttonLabels[row, col] == "÷" || buttonLabels[row, col] == "×" || 
@@ -95,15 +108,15 @@ namespace BasicCalculator
                         button.BackColor = System.Drawing.Color.FromArgb(255, 160, 10); // Orange/yellow
                         button.ForeColor = System.Drawing.Color.White;
                     }
-                    else if (buttonLabels[row, col] == "C")
+                    else if (buttonLabels[row, col] == "C" || buttonLabels[row, col] == "CE" || buttonLabels[row, col] == "DEL")
                     {
-                        // Clear button - light gray
+                        // Clear/Delete buttons - light gray
                         button.BackColor = System.Drawing.Color.FromArgb(192, 192, 192);
                         button.ForeColor = System.Drawing.Color.Black;
                     }
-                    else if (buttonLabels[row, col].StartsWith("M"))
+                    else if (buttonLabels[row, col].StartsWith("M") || buttonLabels[row, col] == "±")
                     {
-                        // Memory buttons - dark gray
+                        // Memory buttons and negate - dark gray
                         button.BackColor = System.Drawing.Color.FromArgb(100, 100, 100);
                         button.ForeColor = System.Drawing.Color.White;
                     }
@@ -128,29 +141,39 @@ namespace BasicCalculator
                     {
                         button.Click += new System.EventHandler(this.EqualsButton_Click);
                     }
-                    else if (buttonLabels[row, col] == "C")
+                    else if (buttonLabels[row, col] == "C" || buttonLabels[row, col] == "CE")
                     {
                         button.Click += new System.EventHandler(this.ClearButton_Click);
+                    }
+                    else if (buttonLabels[row, col] == "DEL")
+                    {
+                        button.Click += new System.EventHandler(this.BackspaceButton_Click);
+                    }
+                    else if (buttonLabels[row, col] == "±")
+                    {
+                        button.Click += new System.EventHandler(this.NegateButton_Click);
+                    }
+                    else if (buttonLabels[row, col].StartsWith("M"))
+                    {
+                        button.Click += new System.EventHandler(this.MemoryButton_Click);
                     }
                     
                     this.Controls.Add(button);
                 }
             }
             
-            // Add backspace button
-            this.backspaceButton = new System.Windows.Forms.Button();
-            this.backspaceButton.Location = new System.Drawing.Point(25, 445);
-            this.backspaceButton.Size = new System.Drawing.Size(300, 40);
-            this.backspaceButton.Text = "Backspace";
-            this.backspaceButton.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            this.backspaceButton.BackColor = System.Drawing.Color.FromArgb(100, 100, 100);
-            this.backspaceButton.ForeColor = System.Drawing.Color.White;
-            this.backspaceButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.backspaceButton.FlatAppearance.BorderSize = 0;
-            this.backspaceButton.Click += new System.EventHandler(this.BackspaceButton_Click);
+            // Create version label
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.versionLabel.Text = "v1.0";
+            this.versionLabel.AutoSize = true;
+            this.versionLabel.Location = new System.Drawing.Point(25, 520);
+            this.versionLabel.ForeColor = System.Drawing.Color.Gray;
+            this.versionLabel.Font = new System.Drawing.Font("Arial", 8F);
             
+            // Add controls to form
+            this.Controls.Add(this.expressionLabel);
             this.Controls.Add(this.displayTextBox);
-            this.Controls.Add(this.backspaceButton);
+            this.Controls.Add(this.versionLabel);
             
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -159,6 +182,7 @@ namespace BasicCalculator
         #endregion
 
         private System.Windows.Forms.TextBox displayTextBox;
-        private System.Windows.Forms.Button backspaceButton;
+        private System.Windows.Forms.Label expressionLabel;
+        private System.Windows.Forms.Label versionLabel;
     }
 }
