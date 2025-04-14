3466fbea2b1ee53638045ce636774c8f6c79411e
karyllecatubay <karyllecatubigcatubay@gmail.com>
2025-04-13 23:11:12 -0700

    Update .program.cs

--- .program.cs
+++ .program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Windows.Forms;
 
-namespace StudentRecordApp
+namespace Catubay_SemifinalActivity2
 {
     static class Program
     {
@@ -13,7 +13,7 @@ namespace StudentRecordApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Catubay_SemifinalActivity1());
+            Application.Run(new Catubay_SemifinalActivity2());
         }
     }
 }
