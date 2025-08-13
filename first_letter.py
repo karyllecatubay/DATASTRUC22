dce5a8773caf95e0e57ca738828542d8e3836fc7
karyllecatubay <karyllecatubigcatubay@gmail.com>
2025-08-13 14:50:29 +0800

    Add files via upload

---
+++ first_letter.py
@@ -0,0 +1,55 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "19fa9569-5589-4d00-b715-28a5c4a79260",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "e07d327b-cb8e-479d-bb82-c59bd37f1b2a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "name = input(\"Tell me your name:\")\n",
+    "\n",
+    "first_letter = name[0].upper()\n",
+    "\n",
+    "print(\"The first letter is: \" + first_letter)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "4e1b71f1-7e28-4e2d-8f12-4ef2d2bbdf1e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
